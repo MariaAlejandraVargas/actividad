d970dbf4be24fff1408b194752a9d3aa8baf156f
MariaAlejandraVargas <aleja1915@hotmail.es>
2022-03-23 07:35:42 -0500

    diplomado

---
+++ Javascript/script.js
@@ -0,0 +1,103 @@
+function mensajeimagen() {
+    alert("OUTSOURCING")
+}
+
+$(".uno").css("color","white")
+$(".dos").css("color","white")
+$(".tres").css("color","white")
+$(".cuatro").css("color","white")
+$(".cinco").css("color","white")
+$(".seis").css("color","white")
+$(".siete").css("color","white")
+$(".ocho").css("color","white")
+$(".nueve").css("color","white")
+$(".diez").css("color","white")
+
+$(".uno").click( function(){
+    $(".uno").css("font-size","30px")
+})
+$(".dos").click( function(){
+    $(".dos").css("font-size","30px")
+})
+$(".tres").click( function(){
+    $(".tres").css("font-size","30px")
+})
+$(".cuatro").click( function(){
+    $(".cuatro").css("font-size","30px")
+})
+$(".cinco").click( function(){
+    $(".cinco").css("font-size","30px")
+})
+$(".seis").click( function(){
+    $(".seis").css("font-size","30px")
+})
+$(".siete").click( function(){
+    $(".siete").css("font-size","30px")
+})
+$(".ocho").click( function(){
+    $(".ocho").css("font-size","30px")
+})
+$(".nueve").click( function(){
+    $(".nueve").css("font-size","30px")
+})
+$(".diez").click( function(){
+    $(".diez").css("font-size","30px")
+})
+
+
+$(".uno").dblclick( function(){
+    $(".uno").css("font-size","15px")
+})
+$(".dos").dblclick( function(){
+    $(".dos").css("font-size","15px")
+})
+$(".tres").dblclick( function(){
+    $(".tres").css("font-size","15px")
+})
+$(".cuatro").dblclick( function(){
+    $(".cuatro").css("font-size","15px")
+})
+$(".cinco").dblclick( function(){
+    $(".cinco").css("font-size","15px")
+})
+$(".seis").dblclick( function(){
+    $(".seis").css("font-size","15px")
+})
+$(".siete").dblclick( function(){
+    $(".siete").css("font-size","15px")
+})
+$(".ocho").dblclick( function(){
+    $(".ocho").css("font-size","15px")
+})
+$(".nueve").dblclick( function(){
+    $(".nueve").css("font-size","15px")
+})
+$(".diez").dblclick( function(){
+    $(".diez").css("font-size","15px")
+})
+
+
+$(".primero").mouseover(function(){
+    $(".primero").css("background", "blue")
+})
+
+$(".segundo").mouseover(function(){
+    $(".segundo").css("background", "blue")
+})
+
+$(".tercero").mouseover(function(){
+    $(".tercero").css("background", "blue")
+})
+
+
+$(".primero").mouseout(function(){
+    $(".primero").css("background", "white")
+})
+
+$(".segundo").mouseout(function(){
+    $(".segundo").css("background", "white")
+})
+
+$(".tercero").mouseout(function(){
+    $(".tercero").css("background", "white")
+})
